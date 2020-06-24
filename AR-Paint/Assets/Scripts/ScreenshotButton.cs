c32c81008b30d0eba3e9d2040333586f44cd25d3
Neil Alexander <na2482r@gre.ac.uk>
2020-06-24 12:34:34 +0100

    Screenshot button added

---
+++ AR-Paint/Assets/Scripts/ScreenshotButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotButton : MonoBehaviour
+{
+    public void TakeScreenshot()
+    {
+        StartCoroutine("Screenshot");
+    }
+
+    IEnumerator Screenshot()
+    {
+        string timeStamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
+
+        string fileName = "Screenshot" + timeStamp + ".png";
+
+        string pathToSave = fileName;
+
+        ScreenCapture.CaptureScreenshot(pathToSave);
+
+        yield return new WaitForEndOfFrame();
+    }
+}
